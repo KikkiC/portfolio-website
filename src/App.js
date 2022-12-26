1e5bb83341e7feb8d0047303b6709066067eeb6a
Kikki Cheung <64998083+KikkiC@users.noreply.github.com>
2022-12-25 23:11:48 -0800

    uploading files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./assets/logo.png"
+import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Resume from './components/resume/Resume';
+import { Blog } from './components/blog/Blog';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <main className='main'>
+        <Home />
+        <About />
+        <Resume />
+        <Blog />
+      </main>
+    </>
+  );
+}
+
+export default App;
