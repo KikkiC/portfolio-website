bf190ac06e487c3c8c31150196af130d0aef8f66
Kikki Cheung <64998083+KikkiC@users.noreply.github.com>
2023-04-24 21:16:38 -0700

    Update Card.jsx

--- src/components/resume/Card.jsx
+++ src/components/resume/Card.jsx
@@ -6,5 +6,6 @@ export const Card = (props) => {
     <span className="timeline__date">{props.year}</span>
     <h3 className="timeline__title">{props.title}</h3>
     <p className="timeline__text">{props.desc}</p>
+    <p className="timeline__skill">{props.skill}</p>
   </div>
 }
