13b560fe2560b87d9081e17a75d36828c9abcfa9
Kikki Cheung <64998083+KikkiC@users.noreply.github.com>
2024-03-06 13:53:34 -0800

    Update HeaderSocials.jsx

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const HeaderSocials = () => {
     return <div className="home__socials">
-        <a href="https://github.com/KikkiC?tab=repositories" className="home__social-link" target="_blank">
+        <a href="https://github.com/KikkiC?tab=repositories" className="home__social-link" target="_blank" rel="noreferrer">
             <i class="fa-brands fa-github-alt"></i>
         </a>
 
-        <a href="https://www.instagram.com/kcontents2022/?hl=en" className="home__social-link" target="_blank">
+        <a href="https://www.instagram.com/kcontents2022/?hl=en" className="home__social-link" target="_blank" rel="noreferrer">
             <i class="fa-brands fa-instagram"></i>
         </a>
 
-        <a href="https://www.linkedin.com/in/kikki-cheung/" className="home__social-link" target="_blank">
+        <a href="https://www.linkedin.com/in/kikki-cheung/" className="home__social-link" target="_blank" rel="noreferrer">
             <i class="fa-brands fa-linkedin"></i>
         </a>
 
-        <a href="https://www.etsy.com/shop/kcontents/?etsrc=sdt" className="home__social-link" target="_blank">
+        <a href="https://www.etsy.com/shop/kcontents/?etsrc=sdt" className="home__social-link" target="_blank" rel="noreferrer">
             <i class="fa-brands fa-etsy"></i>
         </a>
     </div>
