c395b714a3ca40b711ca7970c09032e7d4ee65b2
Kikki Cheung <64998083+KikkiC@users.noreply.github.com>
2024-03-06 13:47:43 -0800

    Update About.jsx
    updated skills

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,46 +14,43 @@ const About = () => {
                 <div className="about__data grid">
                     <div className="about__info">
                         <p className="about__description">
-                            Hi! My name is Kikki and I'm studying Computer Science 
-                            at the University of British Columbia. I'm also a freelance
-                            self-taught artist who owns an Etsy shop called KContents. 
-                            I love combining my art and tech skills to produce cool
-                            products! Feel free to contact me about art commissions 
-                            and my CS background.                  
+                            Hi! My name is Kikki and I'm studying Computer Science at the University of British Columbia. I'm also a freelance
+                            self-taught artist so I love combining my art and tech skills to produce cool
+                            products! Feel free to contact me about art commissions and my CS background.                  
                         </p>
                     </div>
 
                     <div className="about__skills grid">
                         <div className="skills__data">
                             <div className="skills__titles">
-                                <h3 className="skills__name">Visual Art</h3>
+                                <h3 className="skills__name">Quality Assurance</h3>
                                 <span className="skills__number">80%</span>
                             </div>
 
                             <div className="skills__bar">
-                                <span className="skills__percentage canva"></span>
+                                <span className="skills__percentage qa"></span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Visual Art</h3>
+                                <span className="skills__number">75%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage art"></span>
                             </div>
                         </div>
 
                         <div className="skills__data">
                             <div className="skills__titles">
                                 <h3 className="skills__name">Web Development</h3>
-                                <span className="skills__number">70%</span>
+                                <span className="skills__number design">70%</span>
                             </div>
 
                             <div className="skills__bar">
-                                <span className="skills__percentage development"></span>
-                            </div>
-                        </div>
-
-                        <div className="skills__data">
-                            <div className="skills__titles">
-                                <h3 className="skills__name">QA Engineer</h3>
-                                <span className="skills__number design">75%</span>
-                            </div>
-
-                            <div className="skills__bar">
-                                <span className="skills__percentage qa__tester"></span>
+                                <span className="skills__percentage web__dev"></span>
                             </div>
                         </div>
                     </div>
