01157c69afc3c44c93f8a8f61c89bc670b5abe16
Kikki Cheung <64998083+KikkiC@users.noreply.github.com>
2024-03-06 13:54:11 -0800

    Update Data.jsx

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -3,9 +3,9 @@ const Data = [
         id: 1,
         category: "education",
         icon: "icon-graduation",
-        year: "Sept 2019 - Present",
-        title: "Bachelor of Science: Computer Science",
-        desc: "Fourth year standing at the University of British Columbia"
+        year: "Sept 2019 - May 2025",
+        title: "Bachelor of Science: Computer Science Major",
+        desc: "The University of British Columbia, UBC Science Co-op Program",
     },
     {
         id: 2,
@@ -19,28 +19,37 @@ const Data = [
         id: 3,
         category: "experience",
         icon: "icon-briefcase",
-        year: "May 2022 - Present",
-        title: "Freelance Artist, Etsy Shop Owner of KContents",
-        desc: "Utilized digital art programs to create custom artwork and templates. Managed orders and researched trends to optimize listings and descriptions.",
-        skill: "Skills: Canva, Google Drive, Graphic Design, Typography, Design Principles, Customer Service"
+        year: "Sept 2023 - April 2024",
+        title: "QA Analyst Intern @ The University of British Columbia",
+        desc: "Analyzed specifications and user stories to construct manual test cases for Workday IRP Student. Executed tests related to the management of student data, operational reports, student affiliates, and Ad-Hoc communications. Conducted Single User Performance Testing and ensured correct data transfer from the old Student Information System (SIS) to Workday through business data validation. Logged defects against design discrepancies and violations of the report metadata standards.",
+        skill: "Skills: Manual Testing, Jama, Confluence, JIRA, Microsoft Excel, Appian, Workday Integration"
     },
     {
         id: 4,
         category: "experience",
         icon: "icon-briefcase",
-        year: "Sept 2022 - April 2023",
-        title: "Junior QA Engineer Co-op @ Incognito Software Systems",
-        desc: "Wrote automated tests and reduced duplication through shared code. Stablized UI projects and reported ~60 issues for bugs and improvements.",
-        skill: "Skills: Selenium, Java, JavaScript, JUnit, HTML, CSS, Jenkins, Git, Bitbucket, OpenStack, JIRA"
+        year: "May 2022 - Present",
+        title: "Freelance Artist, Etsy Shop Owner of KContents",
+        desc: "Utilized digital art programs to create custom artwork and templates. Managed orders and researched trends to optimize listings and descriptions.",
+        skill: "Skills: Canva, Google Drive, Digital Art, Graphic Design, Typography, Design Principles, Etsy, Customer Service"
     },
     {
         id: 5,
         category: "experience",
         icon: "icon-briefcase",
+        year: "Sept 2022 - April 2023",
+        title: "Junior QA Engineer Co-op @ Incognito Software Systems",
+        desc: "Developed, executed, and maintained the stability of manual and automated tests. Reported ~60 issues for bugs and identified UI design improvements. Investigated logs, updated dependencies, and reduced duplication through refactoring and shared code. Wrote Ansible roles and playbooks to automate service package installations and updates.",
+        skill: "Skills: Selenium, Java, JavaScript, JUnit, HTML, CSS, Jenkins, Git, Bitbucket, OpenStack, Ansible, Gradle, JIRA"
+    },
+    {
+        id: 6,
+        category: "experience",
+        icon: "icon-briefcase",
         year: "Sept 2017 - June 2018",
         title: "Student Tutor @ Robert Cecil Palmer Secondary School",
         desc: "Increased student confidence through attentive instruction in Math, English, and Science. Provided study tips and exam strategies based on students' individual learning style",
-        skill: "Skills: Communication, Tutoring, Flexible Approach, Chinese to English, Active Listening"
+        skill: "Skills: Communication, Private Tutoring, Flexible Approach, Chinese to English Translation, Active Listening"
     },
 ];
 
